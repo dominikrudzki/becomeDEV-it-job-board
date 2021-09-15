5a5a4484be89dc2edcf478329c49b2a36fe150d3
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-15 14:50:29 +0200

    add unique locations in searchbox

--- src/app/core/layout/jobs-page/components/job-search/job-search.component.ts
+++ src/app/core/layout/jobs-page/components/job-search/job-search.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
-  selector: 'app-job-search',
-  templateUrl: './job-search.component.html',
-  styleUrls: ['./job-search.component.scss']
+	selector: 'app-job-search',
+	templateUrl: './job-search.component.html',
+	styleUrls: ['./job-search.component.scss'],
 })
 export class JobSearchComponent implements OnInit {
+	jobLocations: any;
 
-  constructor() { }
+	constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
-  }
+	findUniques(val: any) {
+		let unique = [...new Set(val)];
 
+		return unique;
+	}
+
+	ngOnInit(): void {
+		const locations: string[] = [];
+
+		this.dataService.getJobs().forEach((job) => {
+			locations.push(job.location);
+		});
+
+		this.jobLocations = this.findUniques(locations).sort();
+	}
 }
